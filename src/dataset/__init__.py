b464f949aa5ddff556abd4ab770d55a397eb1223
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2023-12-24 14:28:28 +0000

    Update dataset module

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,37 +1,4 @@
-from dataset.load import ds_root
+from dataset.constants import ORIG_W, ORIG_H
+from dataset.utils import split_dataset, configure_for_performance
 from dataset.dataset import loaded_and_decoded_ds, without_bg_ds, without_bg_and_shadows_ds, triple_ds, augmented_ds
-
-# TODO:
-# - resize images: which scale ?
-# - flatten labels to vector
-# - augment
-# --- remove background ✔️
-# --- remove shadows ✔️
-# --- flip ✔️
-# ----- horizontally ✔️
-# ----- vertically ✔️
-# ----- both ✔️
-# --- color ✔️
-# ----- grayscale ✔️
-# ----- inverse color ✔️
-# ----- remove random channel ✔️
-# --- other
-# ----- add noise
-# ----- blur
-# ----- grid mask
-# --- distortions
-# ----- brightness ✔️
-# ----- saturation ✔️
-# ----- contrast ✔️
-# ----- hue ✔️
-# ----- rotation
-# ----- sharpening
-# --- smart translations (depending on labels)
-# --- synthetic objects
-# - tune performance (https://www.tensorflow.org/guide/data_performance#the_dataset)
-# --- cache
-# --- prefetch
-# --- interleave
-# --- parallel map
-# --- map on batches? - vectorize
-# - prepare TFRecords ?
+from dataset.load import ds_root
