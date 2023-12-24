dd6631ca45736ca7a103e71eaa52edbd778a976f
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2023-12-24 14:26:03 +0000

    Use inline augmentations init

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -6,8 +6,10 @@ import tensorflow as tf
 
 from dataset.load import challenges_pattern, load, decode_image
 from dataset.preprocess import drop_alpha_channel, \
-    remove_background, remove_shadows
-from dataset.augment import random_flip, random_to_grayscale, random_inverse, random_channel_remove
+    remove_background, remove_shadows, restore_shape
+from dataset.augment import random_flip, random_inverse, random_channel_remove, \
+    random_brightness, random_contrast, random_hue, random_saturation, \
+    random_noise, random_blur
 
 
 loaded_and_decoded_ds = tf.data.Dataset.list_files(challenges_pattern) \
@@ -22,14 +24,16 @@ triple_ds = loaded_and_decoded_ds \
     .concatenate(without_bg_ds) \
     .concatenate(without_bg_and_shadows_ds)
 
-# Augmentations init
-_random_flip = random_flip()
-_random_to_grayscale = random_to_grayscale()
-_random_inverse = random_inverse()
-_random_channel_remove = random_channel_remove()
-
-augmented_ds = triple_ds \
-    .map(lambda image, label: _random_flip(image, label)) \
-    .map(_random_channel_remove) \
-    .map(_random_inverse) \
-    .map(_random_to_grayscale)
+augmented_ds = triple_ds.repeat(2) \
+    .map(restore_shape) \
+    .map(random_brightness(max_delta=0.1), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_contrast(lower=0.7, upper=1.3), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_hue(max_delta=0.1), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_saturation(lower=0.7, upper=1.3), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_channel_remove(), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_inverse(), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_flip(), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_blur(), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(random_noise(0.5), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(lambda image, label: (image, tf.reshape(label, [-1])), num_parallel_calls=tf.data.AUTOTUNE) \
+    .map(restore_shape) \
